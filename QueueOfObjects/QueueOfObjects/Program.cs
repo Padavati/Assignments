b2d4f45bb6d0f0aff99dbdd09a8099495f7f4132
Padavati <lavanyavaralu07399@gmail.com>
2021-12-31 18:36:21 +0530

    File added

---
+++ QueueOfObjects/QueueOfObjects/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace QueueOfObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //creating queue
+            Queue q1 = new Queue();
+
+            //taking input from user and store in stack
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Enter name");
+                string name = Console.ReadLine();
+                q1.Enqueue(name);
+                Console.WriteLine("Enter age:");
+                int age = Convert.ToInt32(Console.ReadLine());
+                q1.Enqueue(age);
+            }
+            Console.WriteLine("\n");
+
+            //convert objects to Array
+            Object[] arr = q1.ToArray();
+            Console.WriteLine(arr[0] + "-" + arr[1]);
+            Console.WriteLine(arr[2] + "-" + arr[3]);
+            Console.WriteLine(arr[4] + "-" + arr[5]);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ StackOfObjects/StackOfObjects/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace StackOfObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //creating stack
+            Stack s1 = new Stack();
+
+            //taking input from user and store in stack
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Enter name");
+                string name = Console.ReadLine();
+                s1.Push(name);
+                Console.WriteLine("Enter age:");
+                int age = Convert.ToInt32(Console.ReadLine());
+                s1.Push(age);
+            }
+            Console.WriteLine("\n");
+
+            //convert objects to Array
+            Object[] arr = s1.ToArray();
+            Console.WriteLine(arr[1] + "-" + arr[0]);
+            Console.WriteLine(arr[3] + "-" + arr[2]);
+            Console.WriteLine(arr[5] + "-" + arr[4]);
+
+            Console.ReadKey();
+        }
+    }
+}
